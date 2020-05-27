d874167327b6cc5c11a6c31da08e52a654e70d7c
james-daniels <james.daniels@hotmail.com>
2020-05-27 16:14:10 -0500

    removed private bash completion

--- installKubernetes.sh
+++ installKubernetes.sh
@@ -14,7 +14,6 @@ setupKubectl(){
   mkdir -p $HOME/.kube
   sudo cp -i /etc/kubernetes/admin.conf $HOME/.kube/config
   sudo chown $(id -u):$(id -g) $HOME/.kube/config
-  # kubectl completion bash >> ~/.bashrc
 
   return
 }
